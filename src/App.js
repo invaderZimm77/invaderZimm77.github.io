089d949fb9648a841c59eb32954518b968be2ceb
Terra <bclaycamp@gmail.com>
2022-01-07 14:24:43 -0500

    Tweeks

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="">
       <h1>Terra Claycamp</h1>
-  
+
       <h3>Projects</h3>
       <a href="https://github.com/invaderZimm77/Project_01-gkyWeather">
         Geeky Weather
@@ -16,30 +16,32 @@ function App() {
       </a>
       <br />
       <a href="https://github.com/invaderZimm77/Little_Butler">Lil Butler</a>
-      <h3>Current Projects Workings</h3>
+
+      <h3>Current Projects & Workings</h3>
       <p>
         With a 3D printer in tow, I'm working at setting up a RaspberryPi so I
-        can manage my machine wirelessly{" "}
+        can manage my machine wirelessly.
       </p>
+
       <h3>About Me</h3>
       <p>
         As a Front End Developer with a background in art I face challenges with
         creativity and empathy. From working as a product specialist at a
         medical dispensary to becoming a coffee master at Starbucks, to
-        interning at a Stop Motion Studio, and now as a Software Engineer I
-        pride myself in becoming an expert in my field and constantly learning
-        to evolve with the industry while bringing unique artistic solutions to
-        new problems. I lead with empathy in everything I create holding in my
-        mind the person or persons I am building it for. This care I bring to my
-        clients has made me successful throughout my experience in customer
-        service and it makes me an even stronger developer because I am able to
-        anticipate the needs of the team members interacting with my code as
-        well as the end users engaging with my websites and applications. I look
-        at building code as creating a piece of artwork to give the user an
-        accessible and pleasurable experience.
+        interning at a c Studio, and now as a Software Engineer I pride myself
+        in becoming an expert in my field and constantly learning to evolve with
+        the industry while bringing unique artistic solutions to new problems. I
+        lead with empathy in everything I create holding in my mind the person
+        or persons I am building it for. This care I bring to my clients has
+        made me successful throughout my experience in customer service and it
+        makes me an even stronger developer because I am able to anticipate the
+        needs of the team members interacting with my code as well as the end
+        users engaging with my websites and applications. I look at building
+        code as creating a piece of artwork to give the user an accessible and
+        pleasurable experience.
       </p>
-
-      <FootBox/>
+      <h3>Contact</h3>s
+      <FootBox />
     </div>
   );
 }
