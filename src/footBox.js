9c0b833fdb2cb77ef03ffbd7065f8fd966e7a989
Terra <bclaycamp@gmail.com>
2022-01-07 13:13:13 -0500

    remove more node_modules

---
+++ src/footBox.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './foot.css'
+
+export default function FootBox() {
+	return (
+    <div className='Foot-Box'>
+      <h3>Contact</h3>
+      email: <a href="mailto:tbclaycamp@gmail.com">tbclaycamp@gmail.com</a>
+      <a href="https://github.com/invaderZimm77">Github</a>
+    </div>
+  );
+}
