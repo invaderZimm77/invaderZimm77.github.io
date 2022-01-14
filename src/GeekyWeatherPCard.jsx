15e789e120821f9802b67a7d159f320dcbc4c26c
Terra <bclaycamp@gmail.com>
2022-01-14 10:59:33 -0500

    tweeks to styling

--- src/GeekyWeatherPCard.jsx
+++ src/GeekyWeatherPCard.jsx
@@ -1,23 +1,23 @@
+import "./PrjCard.css";
+
 export default function GeekyWeatherPCard() {
   return (
-    <div>
-      <div>
-        <a
-          href="https://github.com/invaderZimm77/Project_01-gkyWeather"
-          className="project-links"
-        >
+    <div className="project-card">
+      <div className="project-description">
+        Geeky weather is an app that takes City location input and gives a
+        visual feed back of the requested city's current weather and a geeky
+        planet that has similar weather and gives a quip
+      </div>
+      <div className="project-links">
+        <a href="https://github.com/invaderZimm77/Project_01-gkyWeather">
           Geeky Weather - GitHub
         </a>
       </div>
-      <div>
-        <a
-          href="https://invaderzimm77.github.io/Project_01-gkyWeather/"
-          className="project-links"
-        >
+      <div className="project-links">
+        <a href="https://invaderzimm77.github.io/Project_01-gkyWeather/">
           Geeky Weather - Deployed site
         </a>
       </div>
-	  
     </div>
   );
 }
