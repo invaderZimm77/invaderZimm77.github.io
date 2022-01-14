2b655cc4f2801e43c3668ed426dbf0b71eaac26a
Terra <bclaycamp@gmail.com>
2022-01-14 16:50:20 -0500

    added lil butler pcard

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import FootBox from "./footBox";
 import GeekyWeatherPCard from "./GeekyWeatherPCard";
 import SShinePCard from "./SShinePCard";
+import LilButlerPCard from "./LilButlerPCard";
 import AboutMeCard from "./AboutMeCard";
 import Navbar from "react-bootstrap/Navbar";
 import Container from "react-bootstrap/Navbar";
@@ -13,27 +14,26 @@ function App() {
     <div className="">
       <h1>Terra Claycamp</h1>
 
-      {/* <Navbar bg="dark" variant="dark">
+      <Navbar bg="dark" variant="dark">
         <Container>
-          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">About Me</Nav.Link>
-            <Nav.Link href="#pricing">Contact</Nav.Link>
+          <Navbar.Brand href="#home">Terra Claycamp</Navbar.Brand>
+          <Nav className="nav-bar">
+            <Nav.Link href="#about_me">About Me</Nav.Link>
+            <Nav.Link href="#projects">Projects</Nav.Link>
+            <Nav.Link href="#contact">Contact</Nav.Link>
           </Nav>
         </Container>
-      </Navbar> */}
+      </Navbar>
 
-      <h3>Projects</h3>
+      <h3 id="about_me">About Me</h3>
+      <AboutMeCard />
+
+      <h3 id="projects">Projects</h3>
       <GeekyWeatherPCard />
       <SShinePCard />
-
-      <br />
-      <a href="https://github.com/invaderZimm77/Little_Butler">Lil Butler</a>
-
-      <h3>About Me</h3>
-      <AboutMeCard />
-      <h3>Contact</h3>
+      <LilButlerPCard/>
+      
+      <h3 id="contact">Contact</h3>
       <FootBox />
     </div>
   );
---
+++ src/LilButlerPCard.jsx
@@ -0,0 +1,19 @@
+import "./PrjCard.css";
+export default function LilButlerPCard() {
+  return (
+    <div className="project-card">
+      <h4 className="project-title">Lil Butler</h4>
+      <div className="project-description">
+        Little Buttler Is an assitent for Personal Assistants LLC.* . Track the
+        little things about the clients like their likes/dislikes, prefrences.
+        Then the other employees will be able to read those notes. A message
+        board of sorts if you will.
+      </div>
+      <div className="project-links">
+        <a href="https://github.com/invaderZimm77/Little_Butler">
+          Lil Butler - GitHub
+        </a>
+      </div>
+    </div>
+  );
+}
