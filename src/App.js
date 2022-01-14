498a769618c8ccc1994567f1162696b9d2d56b86
Terra <bclaycamp@gmail.com>
2022-01-14 14:28:11 -0500

    clean setup b4 navbar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import './App.css';
-import FootBox from './footBox';
-import GeekyWeatherPCard from './GeekyWeatherPCard';
-import SShinePCard from './SShinePCard';
-import AboutMeCard from './AboutMeCard';
+import "./App.css";
+import FootBox from "./footBox";
+import GeekyWeatherPCard from "./GeekyWeatherPCard";
+import SShinePCard from "./SShinePCard";
+import AboutMeCard from "./AboutMeCard";
+import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Navbar";
+import {Nav} from "react-bootstrap";
+
 
 function App() {
   return (
     <div className="">
       <h1>Terra Claycamp</h1>
-      <nav>NAV BAR</nav>
+
+      {/* <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">About Me</Nav.Link>
+            <Nav.Link href="#pricing">Contact</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar> */}
+
       <h3>Projects</h3>
-      <GeekyWeatherPCard/>
-      <SShinePCard/>
-      
+      <GeekyWeatherPCard />
+      <SShinePCard />
+
       <br />
       <a href="https://github.com/invaderZimm77/Little_Butler">Lil Butler</a>
 
       <h3>About Me</h3>
-      <AboutMeCard/>
+      <AboutMeCard />
       <h3>Contact</h3>
       <FootBox />
     </div>
