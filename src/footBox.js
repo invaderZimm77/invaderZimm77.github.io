ec14abcc4da909b8a0551f2e8f8f580a725d86d0
Terra <bclaycamp@gmail.com>
2022-05-10 11:08:59 -0400

    updated accessibility colors

--- src/footBox.js
+++ src/footBox.js
@@ -7,20 +7,23 @@ export default function FootBox() {
       <div className="email-link">
         <a href="mailto:tbclaycamp@gmail.com">tbclaycamp@gmail.com</a>
       </div>
-      <a className="smIcons" href="https://github.com/invaderZimm77">
-        <img
-          src="/Octocat.png"
-          alt="Github Icon Missing"
-          className="smIcons-img"
-        />
-      </a>
-      <a className="smIcons" href="https://www.linkedin.com/in/terraclaycamp/">
-        <img
-          src="/hd-linkedinlogo.png"
-          alt="LinkedIn"
-          className="smIcons-img"
-        />
-      </a>
+      <div className="SocNet">
+        <a className="smIcons" href="https://github.com/invaderZimm77">
+          <img
+            src="/Octocat.png"
+            alt="Github Icon Missing"
+            className="smIcons-img"
+          />
+        </a>
+        <a className="smIcons"
+          href="https://www.linkedin.com/in/terraclaycamp/">
+          <img
+            src="/hd-linkedinlogo.png"
+            alt="LinkedIn"
+            className="smIcons-img"
+          />
+        </a>
+      </div>
     </div>
   );
 }
