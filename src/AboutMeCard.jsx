1932ef8dda33719c300f3f3500f19b35cf299533
Terra <bclaycamp@gmail.com>
2022-11-11 15:45:24 -0500

    working on content rewrites

--- src/AboutMeCard.jsx
+++ src/AboutMeCard.jsx
@@ -7,9 +7,10 @@ export default function AboutMeCard() {
       continuing to hone my skills to improve user experiences. I take a
       creative approach to learning and value authentic interactions. As a
       career changer, I bring a lot of experience from many walks of life I've
-      lived from being a knowledge leader as a coffee master to working on a
-      team developing bleeding edge motion capture technology. 
-      In my next step I look forward to see where coding takes me; working with great people making a contribution to a team, the world?
+      lived, from being a knowledge leader as a coffee master to working on a
+      team developing bleeding edge motion capture technology. In my next step I
+      look forward to seeing where coding takes me; working with great people
+      making a contribution to a team and hopefully the world.
     </div>
   );
 }
